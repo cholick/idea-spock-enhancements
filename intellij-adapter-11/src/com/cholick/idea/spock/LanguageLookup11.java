b76a8d5095aa1b1fc0778aeb69159417f00a259b
Matt Cholick <cholick@gmail.com>
2014-07-05 17:33:54 -0700

    Switch adapter implementations to Java.

---
+++ intellij-adapter-11/src/com/cholick/idea/spock/LanguageLookup11.java
@@ -0,0 +1,12 @@
+package com.cholick.idea.spock;
+
+import com.intellij.lang.Language;
+import org.jetbrains.plugins.groovy.GroovyFileType;
+
+public class LanguageLookup11 extends LanguageLookup {
+
+    public Language groovy() {
+        return GroovyFileType.GROOVY_LANGUAGE;
+    }
+
+}
---
+++ intellij-adapter-14/src/com/cholick/idea/spock/LanguageLookup14.java
@@ -0,0 +1,12 @@
+package com.cholick.idea.spock;
+
+import com.intellij.lang.Language;
+import org.jetbrains.plugins.groovy.GroovyLanguage;
+
+public class LanguageLookup14 extends LanguageLookup {
+
+    public Language groovy() {
+        return GroovyLanguage.INSTANCE;
+    }
+
+}
